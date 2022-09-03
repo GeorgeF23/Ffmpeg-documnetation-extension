29bf3e8856043aff017d392119913797af712145
George Floarea <floareageorge@gmail.com>
2022-09-03 11:55:20 +0300

    Moved documentation fetching in separate class

---
+++ src/data_fetching/DocumentationFetcher.ts
@@ -0,0 +1,90 @@
+import * as vscode from "vscode";
+import TreeProvider from "../view/TreeProivder";
+import TreeNode from "../view/TreeNode";
+import axios from "axios";
+import { HTMLElement, parse } from "node-html-parser";
+
+export class DocumentationFetcher {
+  private url: string;
+  private treeProvider: TreeProvider;
+  private view: string;
+
+  constructor(url: string, view: string) {
+    this.url = url;
+    this.treeProvider = new TreeProvider();
+    this.view = view;
+
+    vscode.window.registerTreeDataProvider(this.view, this.treeProvider);
+  }
+
+  createTree(root: TreeNode, nodes: HTMLElement[] | undefined) {
+    if (!nodes) {
+      return;
+    }
+    nodes.forEach((node) => {
+      const text = node.querySelector("a")?.text as string;
+      const id = node.querySelector("a")?.attrs["href"].substring(1);
+      const documentationNode = new TreeNode(id, text);
+      root.addChild(documentationNode); // Add current node to it's parrent
+  
+      const children = node
+        .querySelector(":scope > ul")
+        ?.querySelectorAll(":scope > li"); // Nodes with children have ul element
+      if (children && children.length > 0) {
+        this.createTree(documentationNode, children);
+      }
+    });
+  }
+  
+  async fetchDocumentation(): Promise<string> {
+    try {
+      const html = await axios.get(this.url);
+      return html.data;
+    } catch (error) {
+      console.error(`Got an error while fetching documentation: ${error}`);
+      vscode.window.showErrorMessage(
+        `Got an error while fetching documentation: ${error}`
+      );
+      return "";
+    }
+  }
+  
+  async parseDocumentation(
+    rawHtml: string
+  ): Promise<TreeNode | undefined> {
+    try {
+      const root = parse(rawHtml);
+  
+      const tableOfContents = root
+        .querySelector(".contents")
+        ?.querySelector("ul");
+  
+      const nodes = tableOfContents?.querySelectorAll(":scope > li");
+  
+      const rootNode = new TreeNode(undefined, "Ffmpeg filters");
+      rootNode.collapsibleState = vscode.TreeItemCollapsibleState.Expanded;
+      this.createTree(rootNode, nodes);
+  
+      return rootNode;
+    } catch (error) {
+      console.error(`Got an error while parsing documentation: ${error}`);
+      vscode.window.showErrorMessage(
+        `Got an error while parsing documentation: ${error}`
+      );
+    }
+    return undefined;
+  }
+  
+  async refreshDocumentation() {
+    console.log("Refreshing documentation.");
+    const html = await this.fetchDocumentation();
+    const rootNode = await this.parseDocumentation(html);
+    this.treeProvider.setRootNode(rootNode);
+    this.treeProvider.setHtml(html);
+    this.treeProvider.refresh();
+  }
+
+  getProvider(): TreeProvider {
+    return this.treeProvider;
+  }
+}
--- src/extension.ts
+++ src/extension.ts
@@ -1,75 +1,7 @@
 import * as vscode from "vscode";
 import TreeProvider from "./view/TreeProivder";
 import TreeNode from "./view/TreeNode";
-import axios from "axios";
-import { HTMLElement, parse } from "node-html-parser";
-
-function createTree(root: TreeNode, nodes: HTMLElement[] | undefined) {
-  if (!nodes) {
-    return;
-  }
-  nodes.forEach((node) => {
-    const text = node.querySelector("a")?.text as string;
-    const id = node.querySelector("a")?.attrs["href"].substring(1);
-    const documentationNode = new TreeNode(id, text);
-    root.addChild(documentationNode); // Add current node to it's parrent
-
-    const children = node
-      .querySelector(":scope > ul")
-      ?.querySelectorAll(":scope > li"); // Nodes with children have ul element
-    if (children && children.length > 0) {
-      createTree(documentationNode, children);
-    }
-  });
-}
-
-async function fetchDocumentation(): Promise<string> {
-  try {
-    const html = await axios.get("https://ffmpeg.org/ffmpeg-filters.html");
-    return html.data;
-  } catch (error) {
-    console.error(`Got an error while fetching documentation: ${error}`);
-    vscode.window.showErrorMessage(
-      `Got an error while fetching documentation: ${error}`
-    );
-    return "";
-  }
-}
-
-async function parseDocumentation(
-  rawHtml: string
-): Promise<TreeNode | undefined> {
-  try {
-    const root = parse(rawHtml);
-
-    const tableOfContents = root
-      .querySelector(".contents")
-      ?.querySelector("ul");
-
-    const nodes = tableOfContents?.querySelectorAll(":scope > li");
-
-    const rootNode = new TreeNode(undefined, "Ffmpeg filters");
-    rootNode.collapsibleState = vscode.TreeItemCollapsibleState.Expanded;
-    createTree(rootNode, nodes);
-
-    return rootNode;
-  } catch (error) {
-    console.error(`Got an error while parsing documentation: ${error}`);
-    vscode.window.showErrorMessage(
-      `Got an error while parsing documentation: ${error}`
-    );
-  }
-  return undefined;
-}
-
-async function refreshDocumentation(provider: TreeProvider) {
-  console.log("Refreshing documentation.");
-  const html = await fetchDocumentation();
-  const rootNode = await parseDocumentation(html);
-  provider.setRootNode(rootNode);
-  provider.setHtml(html);
-  provider.refresh();
-}
+import { DocumentationFetcher } from "./data_fetching/DocumentationFetcher";
 
 function openFiltersWebview(html: string, target?: string) {
   const panel = vscode.window.createWebviewPanel(
@@ -91,20 +23,18 @@ export async function activate(context: vscode.ExtensionContext) {
     'Congratulations, your extension "ffmpeg-documentation" is now active!'
   );
 
-  const documentationProvider = new TreeProvider();
-  vscode.window.registerTreeDataProvider("main-view", documentationProvider);
-  await refreshDocumentation(documentationProvider);
-
+  const mainFetcher = new DocumentationFetcher('https://www.ffmpeg.org/ffmpeg.html', 'main-view');
+  mainFetcher.refreshDocumentation();
   context.subscriptions.push(
     vscode.commands.registerCommand("ffmpeg-documentation.refresh-data", () =>
-      refreshDocumentation(documentationProvider)
+      mainFetcher.refreshDocumentation()
     )
   );
   context.subscriptions.push(
     vscode.commands.registerCommand(
       "ffmpeg-documentation.open-filter-webview",
       (node?: TreeNode) =>
-        openFiltersWebview(documentationProvider.getHtml(), node?.id)
+        openFiltersWebview(mainFetcher.getProvider().getHtml(), node?.id)
     )
   );
 }
